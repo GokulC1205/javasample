42c31dd77825ac1f50e6d9150878ac995a6d8560
GokulC1205 <141210083+GokulC1205@users.noreply.github.com>
2025-02-27 13:54:05 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String[] args) {
-        System.out.println("Hello Amirdhesh!");
+        System.out.println("Hello Gokul!");
         System.out.println("Welcome to Java.");
     }
 }
